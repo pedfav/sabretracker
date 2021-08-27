2587fe22374ea6597b9271ad4519f36a73668bc2
pedfav <pedfav@gmail.com>
2021-08-26 23:01:11 -0300

    kickoff

---
+++ src/sabretracker.py
@@ -0,0 +1,13 @@
+import requests
+import base64
+import time
+import RPi.GPIO as GPIO
+
+from RPLCD.gpio import CharLCD
+
+GPIO.setwarnings(False)
+lcd = CharLCD(pin_rs=19, pin_rw=None, pin_e=16, pins_data=[21,18,23,24], numbering_mode=GPIO.BOARD, cols=16, rows=2, dotsize=8)
+
+lcd.write_string('Sabretracker')
+lcd.crlf()
+lcd.write_string('Sabretracker')
