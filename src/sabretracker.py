ec2af4ccc160385d5d83aae99cc9ee9c2b652dc4
pedfav <pedfav@gmail.com>
2021-08-27 00:42:55 -0300

    adding lcd

--- src/sabretracker.py
+++ src/sabretracker.py
@@ -7,11 +7,35 @@ from RPLCD.gpio import CharLCD
 
 GPIO.setwarnings(False)
 lcd = CharLCD(pin_rs=19, pin_rw=None, pin_e=16, pins_data=[21,18,23,24], numbering_mode=GPIO.BOARD, cols=16, rows=2, dotsize=8)
-ser = serial.Serial ("/dev/ttyS0", 9600)
+
+ser = serial.Serial ("/dev/ttyS0")
+gpgga_info = "$GPGGA,"
+GPGGA_buffer = 0
+NMEA_buff = 0
+
+def convert_to_degrees(raw_value):
+    decimal_value = raw_value/100.00
+    degrees = int(decimal_value)
+    mm_mmmm = (decimal_value - int(decimal_value))/0.6
+    position = degrees + mm_mmmm
+    position = "%.4f" %(position)
+    return position
 
 while True:
-  try:
     received_data = (str)(ser.readline()) #read NMEA string received
-    print(received_data, "\n")
-  except Exception as e:
-    print(e)
+    GPGGA_data_available = received_data.find(gpgga_info)   #check for NMEA GPGGA string                
+    if (GPGGA_data_available>0):
+        GPGGA_buffer = received_data.split("$GPGGA,",1)[1]  #store data coming after "$GPGGA," string
+        NMEA_buff = (GPGGA_buffer.split(','))
+        nmea_time = []
+        nmea_latitude = []
+        nmea_longitude = []
+        nmea_time = NMEA_buff[0]                    #extract time from GPGGA string
+        nmea_latitude = NMEA_buff[1]                #extract latitude from GPGGA string
+        nmea_longitude = NMEA_buff[3]               #extract longitude from GPGGA string
+        print("NMEA Time: ", nmea_time,'\n')
+        lat = (float)(nmea_latitude)
+        lat = convert_to_degrees(lat)
+        longi = (float)(nmea_longitude)
+        longi = convert_to_degrees(longi)
+        print ("NMEA Latitude:", lat,"NMEA Longitude:", longi,'\n') 
