02a54fb435c2783a008f84899a65a548718f7b0e
pedfav <pedfav@gmail.com>
2021-08-27 01:18:52 -0300

    adding lcd

--- src/sabretracker.py
+++ src/sabretracker.py
@@ -2,16 +2,22 @@ import time
 import serial              
 import sys
 import RPi.GPIO as GPIO
+import pynmea2
 
 from RPLCD.gpio import CharLCD
 
 GPIO.setwarnings(False)
 lcd = CharLCD(pin_rs=19, pin_rw=None, pin_e=16, pins_data=[21,18,23,24], numbering_mode=GPIO.BOARD, cols=16, rows=2, dotsize=8)
-ser = serial.Serial ("/dev/ttyS0", 9600)
+ser=serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.5)
+
 
 while True:
   try:
-    received_data = (str)(ser.readline()) #read NMEA string received
-    print(received_data, "\n")
+    dataout = pynmea2.NMEAStreamReader()
+    newdata = ser.readline()
+    print(newdata)
+
+    #received_data = (str)(ser.readline()) #read NMEA string received
+    #print(received_data, "\n")
   except Exception as e:
     print(e)
