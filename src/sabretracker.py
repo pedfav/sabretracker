9c5939a5ea30d0565d637cfd72841c086ebd58f3
pedfav <pedfav@gmail.com>
2021-08-26 23:12:33 -0300

    adding lcd

--- src/sabretracker.py
+++ src/sabretracker.py
@@ -8,6 +8,8 @@ from RPLCD.gpio import CharLCD
 GPIO.setwarnings(False)
 lcd = CharLCD(pin_rs=19, pin_rw=None, pin_e=16, pins_data=[21,18,23,24], numbering_mode=GPIO.BOARD, cols=16, rows=2, dotsize=8)
 
-lcd.write_string('Sabretracker')
-lcd.crlf()
-lcd.write_string('Sabretracker')
+while True:
+  lcd.write_string('Sabretracker')
+  lcd.crlf()
+  lcd.write_string('Sabretracker')
+  time.sleep(5)
