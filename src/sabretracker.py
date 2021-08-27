e6c031b0beebb669d200d36c286c1fe33f1d2f2f
pedfav <pedfav@gmail.com>
2021-08-27 00:02:00 -0300

    adding lcd

--- src/sabretracker.py
+++ src/sabretracker.py
@@ -10,10 +10,13 @@ lcd = CharLCD(pin_rs=19, pin_rw=None, pin_e=16, pins_data=[21,18,23,24], numberi
 ser = serial.Serial ("/dev/ttyAMA0")
 
 while True:
-  received_data = (str)(ser.readline()) #read NMEA string received
-  print(received_data, "\n")
-  lcd.write_string('Sabretracker')
-  lcd.crlf()
-  lcd.write_string('Sabretracker')
-  print('print lcd')
-  time.sleep(5)
+  try:
+    received_data = (str)(ser.readline()) #read NMEA string received
+    print(received_data, "\n")
+    lcd.write_string('Sabretracker')
+    lcd.crlf()
+    lcd.write_string('Sabretracker')
+    print('print lcd')
+    time.sleep(5)
+  except Exception as e:
+    print(e)
