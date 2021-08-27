032fd588be232d270e659576425381e9b362d9dd
pedfav <pedfav@gmail.com>
2021-08-27 01:09:09 -0300

    adding lcd

--- src/sabretracker.py
+++ src/sabretracker.py
@@ -1,17 +1,22 @@
+import serial
 import time
-import serial              
-import sys
+import string
+import pynmea2
 import RPi.GPIO as GPIO
-
 from RPLCD.gpio import CharLCD
 
 GPIO.setwarnings(False)
 lcd = CharLCD(pin_rs=19, pin_rw=None, pin_e=16, pins_data=[21,18,23,24], numbering_mode=GPIO.BOARD, cols=16, rows=2, dotsize=8)
-ser = serial.Serial ("/dev/ttyS0", 9600)
 
 while True:
-  try:
-    received_data = (str)(ser.readline()) #read NMEA string received
-    print(received_data, "\n")
-  except Exception as e:
-    print(e)
+	port="/dev/ttyS0"
+	ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+	dataout = pynmea2.NMEAStreamReader()
+	newdata=ser.readline()
+
+	if newdata[0:6] == "$GPRMC":
+		newmsg=pynmea2.parse(newdata)
+		lat=newmsg.latitude
+		lng=newmsg.longitude
+		gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
+		print(gps)
