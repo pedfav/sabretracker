0d3a40b9b54d0f0c3f618c55919af54f5da88419
pedfav <pedfav@gmail.com>
2021-08-28 12:57:13 -0300

    adding lcd

--- src/sabretracker.py
+++ src/sabretracker.py
@@ -25,9 +25,9 @@ while True:
       print(gps)
 
       lcd.clear()
-      lcd.write_string(f'Lat={round(lat, 6)}')
+      lcd.write_string(f'Lat={str(lat)[:16]}')
       lcd.cursor_pos=(1,0)
-      lcd.write_string(f'Lng={round(lng, 6)}')
+      lcd.write_string(f'Lng={str(lng)[:16]}')
       time.sleep(0.5)
   except Exception as e:
     print('ops')
