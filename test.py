334b02309526edf529187d715ffed6a4c8ec22f6
pedfav <pedfav@gmail.com>
2021-08-28 12:02:35 -0300

    adding lcd

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ while True:
   dataout = pynmea2.NMEAStreamReader()
   newdata=ser.readline()
   newdata=newdata.decode('utf-8')
-  #print(newdata)
+  print(newdata)
 
   if newdata[0:6] == "$GPRMC":
     newmsg=pynmea2.parse(newdata)
