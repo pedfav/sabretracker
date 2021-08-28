9a93dbdc2ab911955b75db55cbfd81905bfed99c
pedfav <pedfav@gmail.com>
2021-08-28 11:56:42 -0300

    adding lcd

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import pynmea2
 
 while True:
   port="/dev/ttyAMA0"
-  ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+  ser=serial.Serial(port, baudrate=256000, timeout=0.5)
   dataout = pynmea2.NMEAStreamReader()
   newdata=ser.readline()
   newdata=newdata.decode('utf-8')
@@ -15,5 +15,5 @@ while True:
     newmsg=pynmea2.parse(newdata)
     lat=newmsg.latitude
     lng=newmsg.longitude
-    gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
+    gps = "Latitude=" + str(lat) + " and Longitude=" + str(lng)
     print(gps)
