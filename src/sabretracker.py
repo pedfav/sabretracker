44382b9ef3a0434e5601e5b8ac8f0758f0e8a888
pedfav <pedfav@gmail.com>
2021-08-27 23:48:58 -0300

    adding lcd

--- src/sabretracker.py
+++ src/sabretracker.py
@@ -8,7 +8,7 @@ from RPLCD.gpio import CharLCD
 
 GPIO.setwarnings(False)
 lcd = CharLCD(pin_rs=19, pin_rw=None, pin_e=16, pins_data=[21,18,23,24], numbering_mode=GPIO.BOARD, cols=16, rows=2, dotsize=8)
-ser=serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.5)
+#ser=serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.5)
 
 while True:
   try:
@@ -16,7 +16,7 @@ while True:
     lcd.cursor_pos = (2, 0)
     lcd.write('c0ck 5uck3r')
 
-    received_data = ser.readline() #read NMEA string received
-    print(received_data, "\n")
+    #received_data = ser.readline() #read NMEA string received
+    #print(received_data, "\n")
   except Exception as e:
     print(e)
