6e4fbab4c32416359632b65fa31d7fff5b66d3c5
pedfav <pedfav@gmail.com>
2021-08-28 11:53:54 -0300

    adding lcd

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ while True:
   ser=serial.Serial(port, baudrate=9600, timeout=0.5)
   dataout = pynmea2.NMEAStreamReader()
   newdata=ser.readline()
-  newdata.decode('utf-8')
+  newdata=newdata.decode('utf-8')
   print(newdata)
 
   if newdata[0:6] == "$GPRMC":
