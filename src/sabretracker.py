b81278183b41863aac0a7c7a0f4b1523c4203938
pedfav <pedfav@gmail.com>
2021-08-31 13:48:36 -0300

    adding lcd

--- src/sabretracker.py
+++ src/sabretracker.py
@@ -1,14 +1,23 @@
 import time
 import serial              
 import sys
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 import pynmea2
 import uuid
 
 from RPLCD.gpio import CharLCD
 
-GPIO.setwarnings(False)
-lcd = CharLCD(pin_rs=19, pin_rw=None, pin_e=16, pins_data=[21,18,23,24], numbering_mode=GPIO.BOARD, cols=16, rows=2, dotsize=8)
+gpio.setwarnings(False)
+gpio.setmode(gpio.BOARD)
+
+lcd = CharLCD(pin_rs=19, pin_rw=None, pin_e=16, pins_data=[21,18,23,24], numbering_mode=gpio.BOARD, cols=16, rows=2, dotsize=8)
+gpio.setup(15, gpio.IN, pull_up_down = gpio.PUD_DOWN)
+
+def print_to_lcd(first_line, second_line):
+  lcd.clear()
+  lcd.write_string(first_line)
+  lcd.cursor_pos=(1,0)
+  lcd.write_string(second_line)
 
 while True:
   try:
@@ -25,11 +34,8 @@ while True:
       gps = "Latitude=" + str(lat) + " and Longitude=" + str(lng)
       print(gps)
 
-      lcd.clear()
-      lcd.write_string(f'Lat={str(lat)}'[:16])
-      lcd.cursor_pos=(1,0)
-      lcd.write_string(f'Lng={str(lng)}'[:16])
-      time.sleep(0.5)
+      if(gpio.input(15) == 1):
+        print_to_lcd(f'Lat={str(lat)}'[:16], f'Lng={str(lng)}'[:16])
 
     gpsSpeedText = ''
     gpsSpeedText2 = ''
@@ -42,11 +48,8 @@ while True:
       gpsSpeedText2 = "GPRMC: {speed} Kmh".format(speed=data.spd_over_grnd)
       print(gpsSpeedText2)
     
-    lcd.clear()
-    lcd.write_string(gpsSpeedText)
-    lcd.cursor_pos=(1,0)
-    lcd.write_string(gpsSpeedText2)
-    time.sleep(0.5)
+    if(gpio.input(15) == 0):
+      print_to_lcd(gpsSpeedText, gpsSpeedText2)
 
   except Exception as e:
     print(f'ops with error={e}')
