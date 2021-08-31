108768f06b6a1e796611b7907843f90d34b47d18
pedfav <pedfav@gmail.com>
2021-08-30 21:12:47 -0300

    adding lcd

--- src/sabretracker.py
+++ src/sabretracker.py
@@ -35,11 +35,11 @@ while True:
     gpsSpeedText2 = ''
     if newdata.startswith("$GPVTG") :
       data = pynmea2.parse(newdata)
-      gpsSpeedText = "Speed GPVTG: {speed} Kmh".format(speed=data.spd_over_grnd_kmph)
+      gpsSpeedText = "GPVTG: {speed} Kmh".format(speed=data.spd_over_grnd_kmph)
       print(gpsSpeedText)
     if newdata.startswith("$GPRMC") :
       data = pynmea2.parse(newdata)
-      gpsSpeedText2 = "Speed GPRMC: {speed} Kmh".format(speed=data.spd_over_grnd)
+      gpsSpeedText2 = "GPRMC: {speed} Kmh".format(speed=data.spd_over_grnd)
       print(gpsSpeedText2)
     
     lcd.clear()
